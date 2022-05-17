27977ff9a1a3b4edebe9cff96dd1a1ce6af4b5ce
IsaGalo80 <isagalo1980@outlook.com>
2022-05-17 14:36:16 +0000

    generdor de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const btn = document.querySelector("button");
+btn.addEventListener("click", function Excusándome() {
+  let who = ["El perro", "Mi abuela", "Mi tortuga", "Mi canario", "Mi gato"];
+  let what = ["se comió", "aplastó", "robó", "rompió", "ensució"];
+  let when = [
+    "cuando iba a dormir",
+    "cuando llegué",
+    "mientras desayunaba con mi hermano",
+    "mientras pensaba que comprar"
+  ];
+
+  function excuseGenerator(quien, que, cuando) {
+    numQuien = Math.floor(Math.random() * quien.length);
+    numQue = Math.floor(Math.random() * que.length);
+    numCuando = Math.floor(Math.random() * cuando.length);
+    return (
+      quien[numQuien] +
+      " " +
+      que[numQue] +
+      " " +
+      "mi tarea" +
+      " " +
+      cuando[numCuando]
+    );
+
+    function onLoad() {
+      var excusa = document.getElementById("excuse");
+      excusa.innerHTML = excuseGenerator(who, what, when);
+    }
+  }
+});
