f357ac83a5535c52510f875b1fe111ca90f3844f
IsaGalo80 <isagalo1980@outlook.com>
2022-05-21 10:06:58 +0000

    Corregido  el generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,34 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const btn = document.querySelector("button");
-btn.addEventListener("click", function Excusándome() {
-  let who = ["El perro", "Mi abuela", "Mi tortuga", "Mi canario", "Mi gato"];
-  let what = ["se comió", "aplastó", "robó", "rompió", "ensució"];
+window.onload = () => {
+  //write your code here
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
   let when = [
-    "cuando iba a dormir",
-    "cuando llegué",
-    "mientras desayunaba con mi hermano",
-    "mientras pensaba que comprar"
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
   ];
 
-  function excuseGenerator(quien, que, cuando) {
-    numQuien = Math.floor(Math.random() * quien.length);
-    numQue = Math.floor(Math.random() * que.length);
-    numCuando = Math.floor(Math.random() * cuando.length);
-    return (
-      quien[numQuien] +
-      " " +
-      que[numQue] +
-      " " +
-      "mi tarea" +
-      " " +
-      cuando[numCuando]
-    );
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
 
-    function onLoad() {
-      var excusa = document.getElementById("excuse");
-      excusa.innerHTML = excuseGenerator(who, what, when);
-    }
-  }
-});
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+};
